b54a7631d927f22df6ca1d1e9e0e9463a5a72705
James Tang <james@goodnotesapp.com>
2019-11-11 19:39:24 +0800

    Add presentation in code

--- testPopoverViewController/InnerViewController.swift
+++ testPopoverViewController/InnerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  InnerViewController.swift
 //  testPopoverViewController
 //
 //  Created by James Tang on 11/11/2019.
@@ -8,31 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InnerViewController: UIViewController {
 
-    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var presentButton: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func bottomButtonDidClick(_ sender: Any) {
+    @IBAction func presentButtonDidPres(_ sender: Any) {
 //        guard let popover = self.storyboard?.instantiateViewController(withIdentifier: "Popover") else {
 //            return
 //        }
-
         let popover = UIViewController()
         popover.preferredContentSize = CGSize(width: 500, height: 300)
         popover.modalPresentationStyle = .popover
         if let presentation = popover.presentationController as? UIPopoverPresentationController {
             presentation.permittedArrowDirections = [.up, .down]
             presentation.sourceRect = CGRect(x: 0, y: 0, width: 22, height: 22)
-            presentation.sourceView = self.bottomButton
+            presentation.sourceView = self.presentButton
         }
 
-        self.present(popover, animated: true, completion: nil)
+        self.parent?.present(popover, animated: true, completion: nil)
     }
-
 }
-
