1f732b28d06c63b491ef705c482c73162bf515c7
James Tang <james@goodnotesapp.com>
2019-11-19 11:39:42 +0800

    Add UISplitViewController

--- testPopoverViewController/SceneDelegate.swift
+++ testPopoverViewController/SceneDelegate.swift
@@ -17,7 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene), let window = scene.windows.first  else { return }
+        guard let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() else { return }
+
+        let splitViewController = UISplitViewController()
+        splitViewController.viewControllers = [UITableViewController(), viewController]
+        window.rootViewController = splitViewController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
